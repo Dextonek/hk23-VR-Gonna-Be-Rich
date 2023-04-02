578fd8ca5420e954a0d2f1cec98c18b2a48f467f
penkys <erich.penkert@gmail.com>
2023-04-02 03:58:04 +0200

    PiggyBank
    PiggyBank

--- VR Gonna Be Rich/Assets/PiggyBankManager.cs
+++ VR Gonna Be Rich/Assets/PiggyBankManager.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class WalletManager : MonoBehaviour
+public class PiggyBankManager : MonoBehaviour
 {
-    
-    
+    [SerializeField] private TextMeshProUGUI piggyBankValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        piggyBankValue.text = "8000€";
     }
 
     // Update is called once per frame
