c06931946cf64ed408f60e8a680e9b783b87af92
MartinBerka <martin.berka@kosickaakademia.sk>
2023-04-02 03:58:31 +0200

    sg

--- VR Gonna Be Rich/Assets/WalletManager.cs
+++ VR Gonna Be Rich/Assets/WalletManager.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class WalletManager : MonoBehaviour
 {
-    [Se]
+    [SerializeField] private TextMeshProUGUI walletValue;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        walletValue.text = "50€";
     }
 
     // Update is called once per frame
--- VR Gonna Be Rich/Assets/WhiteboardRightManager.cs
+++ VR Gonna Be Rich/Assets/WhiteboardRightManager.cs
@@ -4,23 +4,39 @@ using System.Collections.Generic;
 using System.Globalization;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class WhiteboardRightManager : MonoBehaviour
 {
     private DateTime _date;
 
-    [SerializeField]
-    private TextMeshProUGUI dateField;
+    [SerializeField] private TextMeshProUGUI dateField;
+
+    [SerializeField] private TextMeshProUGUI balanceField;
+
+    [SerializeField] private Button nextDate;
     // Start is called before the first frame update
     void Start()
     {
         _date = DateTime.Today;
         dateField.text = _date.ToString(CultureInfo.CurrentCulture);
+        balanceField.text = "650€";
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (dateField.text != _date.ToString(CultureInfo.CurrentCulture))
+        {
+            dateField.text = _date.ToString(CultureInfo.CurrentCulture);
+        }   
+    }
+
+    public void NextDate()
+    {
+        _date += TimeSpan.FromDays(1);
+        balanceField.text = "500€";
     }
 }
